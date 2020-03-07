cc694e7a3da76315e747dcd5c6475ba722a050c8
Buddy Agyin <agyin3@gmail.com>
2020-03-07 15:53:29 -0700

    setup routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import './App.css';
-import LoginHeader from './components/LoginHeader';
-import Footer from './components/Footer';
+import { Switch, Route} from 'react-router-dom'
+import { PrivateRoute } from './utils/PrivateRoute.js'
+import { WelcomePage } from './components/WelcomPage.js';
+import { ProfessorPage } from './components/ProfessorPage';
+import { StudentPage } from './components/StudentPage';
 
 function App() {
   return (
-    <div>
-      <LoginHeader />
-      <Footer />
-    </div>
+    <Switch>
+      <Route exact path='/'>
+        <WelcomePage />
+      </Route>
+      <Route exact path='/professor/login'>
+        <ProfessorPage />
+      </Route>
+      <Route exact path='/student/login'>
+        <StudentPage />
+      </Route>
+    </Switch>
   );
 }
 
