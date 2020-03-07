37ac84ac3b4b0a6dc6a224dac84c7d0b8d08d2a5
Buddy Agyin <agyin3@gmail.com>
2020-03-07 16:50:03 -0700

    update baseURL

--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export const axiosWithAuth = () => {
     return(
         axios.create({
-            baseURL: 'https://better-professor-app-1.herokuapp.com/',
+            baseURL: 'https://better-professor-app-1.herokuapp.com/api/',
             headers: {
                 authorization: localStorage.getItem('token')
             }
