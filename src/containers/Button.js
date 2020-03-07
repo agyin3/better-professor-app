c87a428240d679792761265d65fc739aafe38ca8
Buddy Agyin <agyin3@gmail.com>
2020-03-07 15:52:35 -0700

    create reusable button component

---
+++ src/containers/Button.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const Button = styled.button`
+    background: ${props => props.background || '#113F67'};
+    color: ${props => props.color || '#F3F9FB'};
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: ${props => props.width || '10rem'};
+    padding: ${props => props.padding || '2% 0'};
+    border-radius: 5px;
+    font-size: ${props => props.size || '1.6rem'};
+
+    &:hover {
+        cursor: pointer;
+        color: #87C0CD;
+    }
+`
