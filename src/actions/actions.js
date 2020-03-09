7380b8e04a7849115f6608d9794f62915aa3f148
Buddy Agyin <agyin3@gmail.com>
2020-03-08 23:44:11 -0600

    add register actions

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,16 +1,50 @@
 import * as types from './types.js'
+import { axiosWithAuth } from '../utils/axiosWithAuth.js'
 
 
 export const userLogin = (type, credentials) => dispatch => {
     dispatch({type: types.LOGIN_START})
     axiosWithAuth()
-        .post(`/auth/login/?type=${type === professor ? 'p' : 's'}`, credentials)
+        .post(`/auth/login/?type=${type === 'professor' ? 'p' : 's'}`, credentials)
         .then(res => {
-            dispatch({type: types.LOGIN_SUCCESS, payload: res.data.data})
+            console.log(res.data)
+            dispatch({type: types.LOGIN_SUCCESS, payload: res.data.data.user})
+            localStorage.setItem('token', res.data.data.token)
+        })
+        .catch(() => {
+            dispatch({type: types.LOGIN_FAIL, payload: 'Incorrect username/password'})
+        })
+}
+
+export const professorRegister = (info) => dispatch => {
+    dispatch({type: types.REGISTER_PROFESSOR_START})
+    axiosWithAuth()
+    .post(`auth/register`, info)
+    .then(res => {
+        dispatch({type: types.REGISTER_PROFESSOR_SUCCESS, payload: res.data.data.user})
+        localStorage.setItem('token', res.data.data.token)
+    })
+    .catch(err => {
+        console.log(err)
+        dispatch({type: types.REGISTER_PROFESSOR_FAIL, payload: 'Username or email is already in the system'})
+    })
+}
+
+export const registerStudent = (info) => dispatch => {
+    dispatch({type: types.REGISTER_STUDENT_START})
+    axiosWithAuth()
+        .put(`/students/${info.id}?register=true`, {
+            username: info.username,
+            password: info.password,
+            email: info.email,
+            registered: true
+        })
+        .then(res => {
+            dispatch({type: types.REGISTER_STUDENT_SUCCESS, payload: res.data.data.student})
             localStorage.setItem('token', res.data.data.token)
         })
         .catch(err => {
             console.log(err)
-            dispatch({type: types.LOGIN_FAIL, payload: 'Invalid username or password'})
+            dispatch({type: types.REGISTER_STUDENT_FAIL, payload: 'Username or email is already in the system'})
         })
 }
