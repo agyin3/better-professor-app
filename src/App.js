dbb4bd80284e57e148a2a13e75f9d7ec62294c70
Buddy Agyin <agyin3@gmail.com>
2020-03-08 23:45:58 -0600

    route to professor signup page

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import './App.css';
 import { Switch, Route} from 'react-router-dom'
 import { PrivateRoute } from './utils/PrivateRoute.js'
 import { WelcomePage } from './components/WelcomPage.js';
-import { ProfessorLogin } from './components/ProfessorLogin';
-import { StudentLogin } from './components/StudentLogin';
-import { StudentSignup } from './components/StudentSignup';
+import { ProfessorLogin } from './components/ProfessorLogin.js';
+import { ProfessorSignup } from './components/ProfessorSignup.js';
+import { StudentLogin } from './components/StudentLogin.js';
+import { StudentSignup } from './components/StudentSignup.js';
 
 function App() {
   return (
@@ -15,6 +16,9 @@ function App() {
       </Route>
       <Route exact path='/professor/login'>
         <ProfessorLogin />
+      </Route>
+      <Route exact path='/professor/signup'>
+        <ProfessorSignup />
       </Route>
       <Route exact path='/student/login'>
         <StudentLogin />
