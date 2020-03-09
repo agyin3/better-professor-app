e16a87156c4afcf0d376173410bbfd97ba4ccab3
Buddy Agyin <agyin3@gmail.com>
2020-03-08 23:45:37 -0600

    create register/login cases

--- src/reducers/professors.js
+++ src/reducers/professors.js
@@ -2,13 +2,44 @@ import * as types from '../actions/types.js'
 
 const initialState = {
     professor: {},
-    students: []
+    students: [],
+    isLoading: false,
+    loginError: '',
+    registerError: ''
 }
 
 export const professors = (state=initialState, {type, payload}) => {
     switch (type) {
+        case types.LOGIN_START:
+        case types.REGISTER_PROFESSOR_START:
+            return{
+                ...state,
+                isLoading: false
+            }
+        case types.LOGIN_SUCCESS: 
+        case types.REGISTER_PROFESSOR_SUCCESS:
+            return {
+                ...state,
+                isLoading: false,
+                professor: {
+                    ...payload
+                },
+                loginError: '',
+                registerError: ''
+            }
+        case types.LOGIN_FAIL: 
+            return {
+                ...state,
+                isLoading: false,
+                loginError: payload
+            }
+        case types.REGISTER_PROFESSOR_FAIL:
+            return {
+                ...state,
+                isLoading: false,
+                registerError: ''
+            }
         default:
             return state
-            break;
     }
 }
--- src/reducers/students.js
+++ src/reducers/students.js
@@ -3,11 +3,43 @@ import * as types from '../actions/types.js'
 const initialState = {
     student: {},
     reminders: [],
-    projects: {}
+    projects: {},
+    isLoading: false,
+    registerError: '',
+    loginError: ''
 }
 
 export const students = (state=initialState, {type, payload}) => {
     switch (type) {
+        case types.REGISTER_STUDENT_START:
+        case types.LOGIN_START:
+            return {
+                ...state,
+                isLoading: true
+            }
+        case types.REGISTER_STUDENT_SUCCESS:
+        case types.LOGIN_SUCCESS:
+            return {
+                ...state,
+                student: {
+                    ...payload
+                },
+                isLoading: false,
+                loginError: '',
+                registerError: ''
+            }
+        case types.REGISTER_STUDENT_FAIL:
+            return {
+                ...state,
+                isLoading:false,
+                registerError: payload
+            }
+        case types.LOGIN_FAIL:
+            return {
+                ...state,
+                isLoading: false,
+                loginError: payload
+            }
         default:
             return state
     }
