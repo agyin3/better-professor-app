2554eacf301712743b4e5ce1262d2dcee0498673
Buddy Agyin <agyin3@gmail.com>
2020-03-09 23:51:58 -0600

    add reducer cases to fetch student and add student

--- src/reducers/professors.js
+++ src/reducers/professors.js
@@ -1,20 +1,39 @@
 import * as types from '../actions/types.js'
 
-const initialState = {
+let initialState = {
     professor: {},
     students: [],
     isLoading: false,
-    loginError: '',
-    registerError: ''
+    errors: {
+        login: '',
+        register: '',
+        fetch: '',
+        add: ''
+    }
 }
 
+const resetErrors = () => {
+    return{
+        login: '',
+        register: '',
+        fetch: '',
+        add: ''
+    }
+}
+
+const persistedData = localStorage.getItem('reduxState')
+if(persistedData){
+    initialState = JSON.parse(persistedData).professors
+}
 export const professors = (state=initialState, {type, payload}) => {
     switch (type) {
         case types.LOGIN_START:
         case types.REGISTER_PROFESSOR_START:
+        case types.FETCH_STUDENTS_START:
+        case types.ADD_STUDENT_START:
             return{
                 ...state,
-                isLoading: false
+                isLoading: true
             }
         case types.LOGIN_SUCCESS: 
         case types.REGISTER_PROFESSOR_SUCCESS:
@@ -24,20 +43,58 @@ export const professors = (state=initialState, {type, payload}) => {
                 professor: {
                     ...payload
                 },
-                loginError: '',
-                registerError: ''
+                errors: resetErrors()
             }
         case types.LOGIN_FAIL: 
             return {
                 ...state,
                 isLoading: false,
-                loginError: payload
+                errors: {
+                    ...state.errors,
+                    login: payload
+                }
             }
         case types.REGISTER_PROFESSOR_FAIL:
             return {
                 ...state,
                 isLoading: false,
-                registerError: ''
+                registerError: payload,
+                errors: {
+                    ...state.errors,
+                    register: payload
+                }
+            }
+        case types.FETCH_STUDENTS_SUCCESS:
+            return {
+                ...state,
+                isLoading: false,
+                errors: resetErrors(),
+                students: [...payload]
+            }
+        case types.FETCH_STUDENTS_FAIL:
+            return {
+                ...state,
+                isLoading: false,
+                errors: {
+                    ...state.errors,
+                    fetch: payload
+                }
+            }
+        case types.ADD_STUDENT_SUCCESS: 
+            return {
+                ...state,
+                isLoading: false,
+                students: [...payload],
+                errors: resetErrors()
+            }
+        case types.ADD_STUDENT_FAIL:
+            return {
+                ...state,
+                isLoading: false,
+                errors: {
+                    ...state.errors,
+                    add: payload
+                }
             }
         default:
             return state
